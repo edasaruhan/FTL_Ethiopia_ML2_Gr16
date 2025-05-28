9dce0866ebcfc17c27f1e9b33e728931059f2855
zol23-g <zelalemwubet93@gmail.com>
2025-05-28 20:43:17 +0300

    build login and dashboard apis

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,11 +1,27 @@
 # backend/accounts/views.py
 from rest_framework import generics, permissions
-from .serializers import CustomUserSerializer, MyTokenObtainPairSerializer
+from .serializers import CustomUserSerializer, EmailTokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from django.contrib.auth import get_user_model
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+User = get_user_model()
 
 class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
     serializer_class = CustomUserSerializer
     permission_classes = [permissions.AllowAny]
 
-class MyTokenObtainPairView(TokenObtainPairView):
-    serializer_class = MyTokenObtainPairSerializer
+class EmailTokenObtainPairView(TokenObtainPairView):
+    serializer_class = EmailTokenObtainPairSerializer
+
+class UserView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({
+            'email': request.user.email,
+            'user_type': request.user.user_type
+        })
